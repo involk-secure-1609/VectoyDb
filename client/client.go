514c856773e5d38e70ede2bbdf1d315c66fbb801
Kevin Jacob <kevintj916@gmail.com>
2025-03-17 04:59:19 +0530

    changed to ignore the env,lol

--- client/client.go
+++ client/client.go
@@ -3,35 +3,34 @@ package client
 import (
 	"context"
 	"log"
-	"os"
+
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
 
 type GeminiClient struct {
 	Client *genai.Client
-	Model *genai.EmbeddingModel
+	Model  *genai.EmbeddingModel
 }
 
-
-func NewGeminiClient() *GeminiClient{
+func NewGeminiClient() *GeminiClient {
 	ctx := context.Background()
 
-	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+	client, err := genai.NewClient(ctx, option.WithAPIKey("AIzaSyC0G1SDBTHP-UFRH9PPkGUpYTFBNM0E6sY"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	em := client.EmbeddingModel("gemini-embedding-exp-03-07")
 	return &GeminiClient{
 		Client: client,
-		Model: em,
+		Model:  em,
 	}
 }
-func (geminiClient *GeminiClient) Embed(key string) ([]float32,error){
+func (geminiClient *GeminiClient) Embed(key string) ([]float32, error) {
 	ctx := context.Background()
 	res, err := geminiClient.Model.EmbedContent(ctx, genai.Text(key))
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return res.Embedding.Values,nil
+	return res.Embedding.Values, nil
 }
